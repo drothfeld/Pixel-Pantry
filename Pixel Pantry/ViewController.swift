8f23e95de0fdbd002711cfaab288bfecf2b0ba92
Dylan Rothfeld <drothfel@gmail.com>
2018-02-26 19:02:50 -0800

    login window screen size

--- Pixel Pantry/ViewController.swift
+++ Pixel Pantry/ViewController.swift
@@ -21,6 +21,14 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+    override func viewDidAppear() {
+        // Removing user's ability to change window size
+        self.view.window?.aspectRatio = NSSize(width: 19, height: 12)
+        // Setting window size
+        self.view.window?.setFrame(NSRect(x:0, y:0, width: 1200, height: 500), display: true)
+        self.view.window?.minSize = NSSize(width: 1000, height: 300)
+    }
 
 
 }
