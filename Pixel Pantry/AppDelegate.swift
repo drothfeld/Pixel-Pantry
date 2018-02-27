b5a228253331133642a123cd8ad469191c819dce
Dylan Rothfeld <drothfel@gmail.com>
2018-02-26 18:36:32 -0800

    inital commit

---
+++ Pixel Pantry/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pixel Pantry
+//
+//  Created by Dylan Rothfeld on 2/26/18.
+//  Copyright © 2018 Dylan Rothfeld. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Pixel Pantry/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pixel Pantry
+//
+//  Created by Dylan Rothfeld on 2/26/18.
+//  Copyright © 2018 Dylan Rothfeld. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
