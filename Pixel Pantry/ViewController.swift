37fa2fd53e8091976cab5ddc877291c580c7981b
Dylan Rothfeld <drothfel@gmail.com>
2018-02-27 16:24:03 -0800

    set inital frame constraints for login sub-view

--- Pixel Pantry/ViewController.swift
+++ Pixel Pantry/ViewController.swift
@@ -9,11 +9,12 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    @IBOutlet weak var LoginView: NSView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view
+        self.view.wantsLayer = true
     }
 
     override var representedObject: Any? {
@@ -24,12 +25,38 @@ class ViewController: NSViewController {
     
     override func viewDidAppear() {
         // Removing user's ability to change window size
-        self.view.window?.aspectRatio = NSSize(width: 19, height: 12)
+        self.view.window?.aspectRatio = NSSize(width: 18, height: 12)
         // Setting window size
-        self.view.window?.setFrame(NSRect(x:0, y:0, width: 1200, height: 500), display: true)
-        self.view.window?.minSize = NSSize(width: 1000, height: 300)
+        self.view.window?.minSize = NSSize(width: 1200, height: 800)
+        self.view.window?.setFrame(NSRect(x:0, y:0, width: 1200, height: 800), display: true)
+        // Removing title bar text and making translucent
+        self.view.window?.styleMask.insert(NSWindow.StyleMask.unifiedTitleAndToolbar)
+        self.view.window?.styleMask.insert(NSWindow.StyleMask.fullSizeContentView)
+        self.view.window?.styleMask.insert(NSWindow.StyleMask.titled)
+        self.view.window?.toolbar?.isVisible = false
+        self.view.window?.titleVisibility = .hidden 
+        self.view.window?.titlebarAppearsTransparent = true
+        // Setting background window colorf
+        self.view.window?.backgroundColor = NSColor.white
+        
+        setupLoginView()
+    }
+    
+    // Drawing login view
+    func setupLoginView() {
+        LoginView.backgroundColor(color: NSColor.systemBlue)
+        LoginView.alphaValue = 0.30
+        LoginView.frame.origin = CGPoint(x: (self.view.frame.width/2) - (LoginView.frame.width/2), y: self.view.frame.height/4)
+        LoginView.setFrameSize(NSSize(width: 400, height: 500))
     }
 
 
+}
+
+extension NSView {
+    func backgroundColor(color: NSColor) {
+        wantsLayer = true
+        layer?.backgroundColor = color.cgColor
+    }
 }
 
